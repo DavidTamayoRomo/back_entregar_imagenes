e81ee26e1a0d2ac983b2996ea29b728ff1a05c0f
dtamayo <franklin.tamayo@quito.gob.ec>
2023-05-23 11:14:06 -0500

    cambios finales

---
+++ server/config/docs.js
@@ -0,0 +1,16 @@
+const swaggerDefinition = {
+  swagger: '2.0',
+  info: {
+    title: 'Checklist API ARTES',
+    version: '1.0.0',
+    description: 'Checklist API Documentetion'
+  },
+  basePath: '/api/',
+  schemes: ['http'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+};
+
+module.exports = {
+  swaggerDefinition
+}
